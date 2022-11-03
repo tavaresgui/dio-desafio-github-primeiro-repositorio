8b82f3bd1ccecc2dddcba7b0bafb14a53cdfefb6
tavaresgui <guilherme.gts10@gmail.com>
2022-11-03 11:43:26 -0300

    Inclusão de um projeto de uma agenda usando python e django

---
+++ Projeto Python-Django/agenda/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from core.models import Evento
+
+# Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('id','titulo', 'data_evento', 'data_criação')
+    list_filter = ('usuario','data_evento',)
+admin.site.register(Evento, EventoAdmin)
